e74ad8aeb68091d38f0dfb58302f4ef561bac56e
Arsham Mehrani <Arsham512@gmail.com>
2022-03-08 17:37:47 -0800

    clean up code and routings

--- main.py
+++ main.py
@@ -14,39 +14,75 @@ from flask_debugtoolbar import DebugToolbarExtension
 import os
 
 
-ratemybroncoDB = mysql.connector.connect(host="localhost", user="root", database="ratemybronco")
-mycursor = ratemybroncoDB.cursor()
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.getenv("key")
 
-app.debug = True
+### MySQL connector and set up
+ratemybroncoDB = mysql.connector.connect(host="localhost", user="root", database="ratemybronco")
+mycursor = ratemybroncoDB.cursor()
 
-toolbar = DebugToolbarExtension(app)
 
+### Landing page routing
 @app.route("/")
 def landing():
   return render_template("index.html")
 
-@app.route("/professors")
-def professors():
-  mycursor.execute("SELECT * FROM Professor") # get all th professors -> make this better
-  myresult = mycursor.fetchall()
-  for i in myresult:
-    print(i)
-        
-
-@app.route("/courses")
-def courses():
-  return "Courses Page"
 
 
-@app.route("/courses/card")
-def ratings():
-  return "Handle the Cards combination of each professor and class that are going to be unique"
+
+### Search page routing and support
+names = [] # To be replaced with database access
+@app.route("/search", methods=["GET","POST"]) 
+def search():
+  if request.method == "GET":
+    return render_template("search.html", professors=names)
+  
+  # check the if there is a number in the query then search the courses
+  professor = request.form.get("professor")
+  if not professor:
+    return "output blank"
+
+  names.append(professor)
+
+  return redirect("/search")
+
+@app.route("/professor/<professor>")
+def professor_page (name, course):
+  pass
 
 
+### add raiting to the database with a SQL
+@app.route("/review", methods=["POST", "GET"])
+def add_rating():
+
+  if request.method == "GET":
+    return render_template("addRating.html")
+
+  # Add server side checking
+  # get all the data from the field
+  ProfessorName = request.form.get("ProfessorName")
+  courseName = request.form.get("courseName")
+  Semester = request.form.get("Semester")
+  Rating = request.form.get("Rating")
+  Comment = request.form.get("Comment")
+  
+  # no need to call an html or redirect
+  # compile it into a csv
+  user_rating = f'"{ProfessorName}" , "{courseName}", "{Semester}", "{Rating}", "{Comment}"' # put in format with commas so we can add it as an sql_command
+  sql_command  = f"INSERT INTO cards (ProfessorName, Class, Semester, Rating, Comment) VALUES ({user_rating});" # add a new row into cards table which we will need to create.
+
+  # Execute this command, expecting no returns.
+  mycursor.execute(sql_command)
+
+### Thank you page or the submitted page
+@app.route("/submitted", method=["GET"])
+def submitted():
+  return "Thank You" 
+
+
+
+### Return the grade disbursement for the displaying of the data
 @app.route("/grade-disbursements")
 def grades():
   # Loading irirs dataset
@@ -71,46 +107,6 @@ def grades():
   return f"Grade Disbursement Page {display(df)} <img src='data:image/png;base64,{data}'/>"
 
 
-names = [] # To be replaced with database access
-@app.route("/search", methods=["GET","POST"]) 
-def search():
-  if request.method == "GET":
-    return render_template("search.html", professors=names)
-    
-  professor = request.form.get("professor")
-  if not professor:
-    return "output blank"
-
-  names.append(professor)
-
-  return redirect("/search")
-
-@app.route("/review", methods=["GET"])
-def review():
-  return "Review form" #Replace with Review Page
-
-@app.route("/submitted", method=["GET"])
-def submitted():
-  return "Thank You" 
-
-@app.route("/add-rating", methods=["POST"])
-def add_rating():
-  # Add server side checking
-  # get all the data from the field
-  ProfessorName = request.form.get("ProfessorName")
-  Class = request.form.get("Class")
-  Semester = request.form.get("Semester")
-  Rating = request.form.get("Rating")
-  Comment = request.form.get("Comment")
-  
-  # no need to call an html or redirect
-  # compile it into a csv
-  user_rating = f'"{ProfessorName}" , "{Class}", "{Semester}", "{Rating}", "{Comment}"' # put in format with commas so we can add it as an sql_command
-  sql_command  = f"INSERT INTO cards (ProfessorName, Class, Semester, Rating, Comment) VALUES ({user_rating});" # add a new row into cards table which we will need to create.
-
-  # Execute this command, expecting no returns.
-  mycursor.execute(sql_command)
-
-
+### Run if main
 if __name__ == "__main__":
-  app.run()
+  app.run(DEBUG=True)
